40aa8b3215f1453dbdf5601479243536d0c0c507
Travis <travishill1@gmail.com>
2019-03-24 21:20:43 -0500

    html added for survey page, custom friends added to array, modal works correctly.  deploy to heroku

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
-        name:"Ahmed",
-        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name:"Frodo",
+        photo:"https://i2.wp.com/www.starkafterdarkonline.com/wp-content/uploads/2015/11/lotr-frodo-950x570.jpg",
         scores:[
             5,
             1,
@@ -14,7 +14,109 @@ var friends = [
             4,
             1
           ]
-    }
+    },
+
+    {
+        name: "Bob Buscemi",
+        photo: "https://hypixel.net/proxy/BcmjTMrMwQR6yFmcqGspjJbCbVEtmkvvuMAc4ddV7OMP42CmoDZSg5PEqlvnmjgI6P9r%2BDAgqjC%2ByDhRDIGRLVvOlSdEngluY4yED7IDMWFkOT3UQwC9D1zoAQEgi%2BveZttS%2FJd0/image.png",
+        scores: [
+        4,
+        2,
+        5,
+        1,
+        3,
+        2,
+        2,
+        1,
+        3,
+        2
+        ]
+        },
+
+        {
+        name: "Harald Hardrada",
+        photo: "https://www.planetfigure.com/attachments/0a-jpg.283800/",
+        scores: [
+        5,
+        2,
+        2,
+        2,
+        4,
+        1,
+        3,
+        2,
+        5,
+        5
+        ]
+        },
+
+        {
+        name: "The Count",
+        photo: "https://cnet4.cbsistatic.com/img/SernYbT3H7lh1J6FWEBZd3ancK8=/970x0/2014/12/01/9bcaf58e-5f17-46b9-82a1-546898aad693/count.jpg",
+        scores: [
+        3,
+        3,
+        4,
+        2,
+        2,
+        1,
+        3,
+        2,
+        2,
+        3
+        ]
+        },
+
+        {
+        name: "Dominar Rygel XVI",
+        photo: "https://pbs.twimg.com/profile_images/901859346664931328/1NQTd145_400x400.jpg",
+        scores: [
+        4,
+        3,
+        4,
+        1,
+        5,
+        2,
+        5,
+        3,
+        1,
+        4
+        ]
+        },
+
+        {
+        name: "Griswold the Blacksmith",
+        photo: "https://vignette.wikia.nocookie.net/diablo/images/0/05/Griswold.jpg/revision/latest?cb=20080325205320",
+        scores: [
+        4,
+        4,
+        2,
+        3,
+        2,
+        2,
+        3,
+        2,
+        4,
+        5
+        ]
+        },
+
+        {
+        name: "Lucifer Sam",
+        photo: "https://i.ytimg.com/vi/tLhepbmpsUI/hqdefault.jpg",
+        scores: [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+        ]
+        }
   ];
 
   module.exports = friends;
